8e483ff684e36347f5e593978f853dcdb5da58dd
ruicorte <ruicorte@gmail.com>
2020-04-18 17:16:26 +0100

    lady loading Posts with suspense - without routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React, { Component, Suspense } from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+// import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
 // import Posts from './containers/Posts';
 import User from './containers/User';
-import Welcome from './containers/Welcome';
+// import Welcome from './containers/Welcome';
 
 const Posts = React.lazy(() => import('./containers/Posts'));
 
 class App extends Component {
+  state = {
+    showPosts: false
+  }
+
+  toggleHandle = () => {
+    this.setState(prevState => {
+      return {
+        showPosts: !prevState.showPosts
+      }
+    });
+  }
+
   render() {
     return (
-      <BrowserRouter>
-        <React.Fragment>
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          {/* <Route path="/posts" component={Posts} /> */}
-          <Route path="/posts" render={() => <Suspense fallback={<div>Loading...</div>}><Posts /></Suspense>} />
-        </React.Fragment>
-      </BrowserRouter>
+      <React.Fragment>
+        <button onClick={this.toggleHandle}>Toggle</button>
+        {this.state.showPosts ? <Suspense fallback={<div>Loading...</div>}><Posts /></Suspense> : <User />}
+      </React.Fragment>
+
+      // <BrowserRouter>
+      //   <React.Fragment>
+      //     <nav>
+      //       <NavLink to="/user">User Page</NavLink> |&nbsp;
+      //       <NavLink to="/posts">Posts Page</NavLink>
+      //     </nav>
+      //     <Route path="/" component={Welcome} exact />
+      //     <Route path="/user" component={User} />
+      //     {/* <Route path="/posts" component={Posts} /> */}
+      //     <Route path="/posts" render={() => <Suspense fallback={<div>Loading...</div>}><Posts /></Suspense>} />
+      //   </React.Fragment>
+      // </BrowserRouter>
     );
   }
 }
